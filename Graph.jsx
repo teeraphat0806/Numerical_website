97b7de1e227a1592f24e41a6395a2059e950644a
kirati1832 <65143196+kirati1832@users.noreply.github.com>
2023-10-15 15:51:20 +0700

    Update Graph.jsx

--- Graph.jsx
+++ Graph.jsx
@@ -4,18 +4,18 @@ import "mafs/font.css";
 import { Mafs, Coordinates, Plot, Theme ,Point} from "mafs";
 import { pow,sqrt } from 'mathjs';
 
-const Show_graph = ({func,x,y}) => {
-    const [equation, setEquation] = useState(func);
-    
+function Show_graph({func,x,y})  {
+   
     return (
       <div>
         <Mafs>
           <Coordinates.Cartesian />
-          <Plot.OfX y={(x) => eval(equation)} color={Theme.blue} />
+          <Plot.OfX y={(x) => eval(func)} color={Theme.blue} />
           <Point x={x} y={y} color={Theme.red}/>
         </Mafs>
      
       </div>
     );
   }
+
   export default Show_graph;
