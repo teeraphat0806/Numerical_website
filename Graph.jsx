6564254bf67eacaa5c717137031eebb3a2b4b42a
Teeraphat Jirapasnitilet <110732968+teeraphat0806@users.noreply.github.com>
2023-10-18 09:57:58 +0700

    Update Graph.jsx

--- Graph.jsx
+++ Graph.jsx
@@ -4,11 +4,12 @@ import "mafs/font.css";
 import { Mafs, Coordinates, Plot, Theme ,Point} from "mafs";
 import { pow,sqrt } from 'mathjs';
 
-function Show_graph({func,x,y})  {
-   
+const Show_graph = ({func,x,y}) => {
+
+    console.log(func);
     return (
       <div>
-        <Mafs>
+        <Mafs  zoom={{ min: 0.1, max: 2 }} viewBox={{x:[x-2,x+2],y:[y-2,y+2]}}>
           <Coordinates.Cartesian />
           <Plot.OfX y={(x) => eval(func)} color={Theme.blue} />
           <Point x={x} y={y} color={Theme.red}/>
@@ -17,5 +18,4 @@ function Show_graph({func,x,y})  {
       </div>
     );
   }
-
   export default Show_graph;
